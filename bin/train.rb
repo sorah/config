4f81feccffe9f60c74bf20e09ac4a48c18800651
Shota Fukumori (sora_h) <sorah@tubusu.net>
2012-08-05 19:32:28 +0900

    show all traininfos, lines and reasons

--- bin/train.rb
+++ bin/train.rb
@@ -10,7 +10,10 @@ if File.exist?(CACHE) && (Time.now - File::Stat.new(CACHE).mtime) <= 300
 end
 
 train_info = open('http://traininfo.jreast.co.jp/train_info/kanto.aspx','r',&:read)
-m = train_info.scan(/宇都宮線|湘南新宿ライン|高崎線|埼京線|山手線/).flatten
+#m = train_info.scan(/宇都宮線|湘南新宿ライン|高崎線|埼京線|山手線/).flatten.uniq
+m = train_info.scan(%r{<td align="left" bgcolor="#FF6633" nowrap="nowrap"><font color="White"><b><font class="px12" size="3"><a href="history.aspx.*?">(.+?)</a></font></b></font></td><td align="center" bgcolor="#FF6633" nowrap="nowrap"><font color="White"><b><font class="px12" size="3">(.+?)</font>}).uniq.map{|x| x.join(":") }
 result = m.empty? ? "" : " #{m.join(", ")} "
 open(CACHE, 'w'){|io| io.print result }
 print result
+
+
