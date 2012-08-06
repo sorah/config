a0791ccba5d3a1b158e79a639208265501f91107
Shota Fukumori <sorah@cookpad.com>
2012-08-06 16:12:51 +0900

    fix regexp

--- bin/train.rb
+++ bin/train.rb
@@ -11,7 +11,7 @@ end
 
 train_info = open('http://traininfo.jreast.co.jp/train_info/kanto.aspx','r',&:read)
 #m = train_info.scan(/宇都宮線|湘南新宿ライン|高崎線|埼京線|山手線/).flatten.uniq
-m = train_info.scan(%r{<td align="left".*?>(?:<font color="White"><b>)?<font class="px12" size="3"><a href="history\.aspx.*?">(.+?)</a>(?:</font></b></font></td>)?<td align="center".*?>(?:<font class="White"><b>)?<font class="px12" size="3">(.+?)</font>}).uniq.map{|x| x.join(":") }
+m = train_info.scan(%r{<td align="left".*?>(?:<font color="White"><b>)?<font class="px12" size="3"><a href="history\.aspx.*?">(.+?)</a>(?:</font></b>)?</font></td><td align="center".*?>(?:<font class="White"><b>)?<font class="px12" size="3">(.+?)</font>}).uniq.map{|x| x.join(":") }
 result = m.empty? ? "" : " #{m.join(", ")} "
 open(CACHE, 'w'){|io| io.print result }
 print result
