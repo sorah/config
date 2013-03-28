0bddb1e82fee5cf27c8f14b59753cef7598d9d70
Shota Fukumori (sora_h) <her@sorah.jp>
2013-03-29 04:57:51 +0900

    Replace Dropbox full path to "-"

--- script/cdd_title.rb
+++ script/cdd_title.rb
@@ -1,4 +1,4 @@
 a = ENV["PWD"];
-a = a.sub(ENV["HOME"],"~").split(/\//); a << "/" if a.empty?
+a = a.sub(ENV["HOME"],"~").sub(%r{^~/Dropbox/Codes}, '_').split(/\//); a << "/" if a.empty?
 print (a.size > 4 ? a[0..-2].map{|x| x[0] } << a[-1] : a).join("/")
 
