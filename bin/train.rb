86783b8d857480bad914396c6d5fd811cb73a943
Shota Fukumori (sora_h) <her@sorah.jp>
2013-03-29 04:56:44 +0900

    Support Ruby 1.8

--- bin/train.rb
+++ bin/train.rb
@@ -9,14 +9,15 @@ if File.exist?(CACHE) && (Time.now - File::Stat.new(CACHE).mtime) <= 300
   exit
 end
 
+
 train_info = open('http://traininfo.jreast.co.jp/train_info/kanto.aspx','r',&:read)
+unless ''.respond_to?(:encode)
+  require 'kconv'
+  train_info = train_info.toutf8
+end
+
 #m = train_info.scan(/宇都宮線|湘南新宿ライン|高崎線|埼京線|山手線/).flatten.uniq
 m = train_info.scan(%r{<td align="left".*?>(?:<font color="White"><b>)?<font class="px12" size="3">(?:<a href="history\.aspx.*?">)?(.+?)(?:</a>)?(?:</font></b>)?</font></td><td align="center".*?>(?:<font class="White"><b>)?<font class="px12" size="3">(.+?)</font>}).uniq.map{|x| x.join(":").gsub(/&nbsp;/,'') }
 result = m.empty? ? "" : " #{m.join(", ")} "
 open(CACHE, 'w'){|io| io.print result }
 print result
-
-
-
-
-
