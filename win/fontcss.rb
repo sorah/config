03e4ab6fecbd00c923f76ff5acf1875d14a754fe
Sorah Fukumori <her@sorah.jp>
2025-04-26 00:14:54 +0900

    win/fontcss: Variable font aware

--- win/fontcss.rb
+++ win/fontcss.rb
@@ -15,6 +15,7 @@ FONT_TO_OVERRIDE = [
   "Hiragino Kaku Gothic Pro",
   "Hiragino Maru Gothic Pro",
 ]
+VF = %w(Noto-Sans-JP)
 SRC = [
   'NotoSansJP',
   'NotoSansCJKjp',
@@ -33,18 +34,20 @@ WEIGHTS = {
 
 FONT_TO_OVERRIDE.each do |font_spec|
   WEIGHTS.each do |psname, weight|
+    locals = [*VF, *SRC.map { |local| "#{local}-#{psname}" }]
     puts <<~EOF
       @font-face {
           font-family: "#{font_spec}";
           font-weight: #{weight};
-          src: #{SRC.map { |local| %|local("#{local}-#{psname}")| }.join(', ')}
+          src: #{locals.map { |local| %|local("#{local}")| }.join(', ')}
       }
     EOF
   end
+  locals = [*VF, *SRC.map { |local| "#{local}-Regular" }]
   puts <<~EOF
     @font-face {
         font-family: "#{font_spec}";
-        src: #{SRC.map { |local| %|local("#{local}-Regular")| }.join(', ')}
+        src: #{locals.map { |local| %|local("#{local}")| }.join(', ')}
     }
   EOF
 end
@@ -54,7 +57,7 @@ puts <<~EOF
     font-family: "Segoe UI";
     font-weight: 400;
     font-style: normal;
-   src: local("NotoSansJP-Regular"), local("NotoSansCJKjp-Regular"), local("SourceHanSans-Regular"), local("Noto Sans CJK JP"), local("LINESeedJP_OTF"), local("LINESeedJP_OTF-Regular"),  local("BIZ-UDPGothic"), local("YuGothic-Medium");
+   src: local("Noto-Sans-JP"), local("NotoSansJP-Regular"), local("NotoSansCJKjp-Regular"), local("SourceHanSans-Regular"), local("Noto Sans CJK JP"), local("BIZ-UDPGothic"), local("YuGothic-Medium");
     unicode-range: U+30??, U+3400-4DB5, U+4E00-9FCB, U+F900-FA6A, U+2E80-2FD5, U+FF5F-FF9F, U+31F0-31FF, U+3220-3243, U+3280-337F, U+FF01-FF5E;
 }
 
@@ -62,61 +65,61 @@ puts <<~EOF
     font-family: "Segoe UI";
     font-weight: 200;
     font-style: normal;
-    src:  local("SegoeUI-Light"), local("SourceHanSans-Light"), local("NotoSansJP-Light"), local("NotoSansCJKjp-Light");
+    src:  local("SegoeUI-Light"), local("SourceHanSans-Light"), local("Noto-Sans-JP"), local("NotoSansJP-Light"), local("NotoSansCJKjp-Light");
 }
 @font-face {
     font-family: "Segoe UI";
     font-weight: 300;
     font-style: normal;
-    src:  local("SegoeUI-Semilight"), local("SourceHanSans-Semilight"), local("NotoSansJP-Medium"), local("NotoSansCJKjp-Medium");
+    src:  local("SegoeUI-Semilight"), local("SourceHanSans-Semilight"), local("Noto-Sans-JP"), local("NotoSansJP-Medium"), local("NotoSansCJKjp-Medium");
 }
 @font-face {
     font-family: "Segoe UI";
     font-weight: 400;
     font-style: normal;
-    src:  local("SegoeUI"), local("SourceHanSans-Regular"), local("NotoSansJP-Regular"), local("NotoSansCJKjp-Regular");
+    src:  local("SegoeUI"), local("SourceHanSans-Regular"), local("Noto-Sans-JP"), local("NotoSansJP-Regular"), local("NotoSansCJKjp-Regular");
 }
 @font-face {
     font-family: "Segoe UI";
     font-weight: 600;
     font-style: normal;
-    src:  local("SegoeUI-Semibold"), local("SourceHanSans-SemiBold"), local("NotoSansJP-SemiBold"), local("NotoSansCJKjp-SemiBold");
+    src:  local("SegoeUI-Semibold"), local("SourceHanSans-SemiBold"), local("Noto-Sans-JP"), local("NotoSansJP-SemiBold"), local("NotoSansCJKjp-SemiBold");
 }
 @font-face {
     font-family: "Segoe UI";
     font-weight: 700;
     font-style: normal;
-    src:  local("SegoeUI-Bold"), local("SourceHanSans-Bold"), local("NotoSansJP-Bold"), local("NotoSansCJKjp-Bold");;
+    src:  local("SegoeUI-Bold"), local("SourceHanSans-Bold"),local("Noto-Sans-JP"),  local("NotoSansJP-Bold"), local("NotoSansCJKjp-Bold");;
 }
 
 @font-face {
     font-family: "Segoe UI";
     font-weight: 200;
     font-style: italic;
-    src:  local("SegoeUI-LightItalic"), local("SourceHanSans-Light"), local("NotoSansJP-Light"), local("NotoSansCJKjp-Light");
+    src:  local("SegoeUI-LightItalic"), local("SourceHanSans-Light"), local("Noto-Sans-JP"), local("NotoSansJP-Light"), local("NotoSansCJKjp-Light");
 }
 @font-face {
     font-family: "Segoe UI";
     font-weight: 300;
     font-style: italic;
-    src:  local("SegoeUI-SemilightItalic"), local("SourceHanSans-Semilight"), local("NotoSansJP-Medium"), local("NotoSansCJKjp-Medium");
+    src:  local("SegoeUI-SemilightItalic"), local("SourceHanSans-Semilight"), local("Noto-Sans-JP"), local("NotoSansJP-Medium"), local("NotoSansCJKjp-Medium");
 }
 @font-face {
     font-family: "Segoe UI";
     font-weight: 400;
     font-style: italic;
-    src:  local("SegoeUI-Italic"), local("SourceHanSans-Regular"), local("NotoSansJP-Regular"), local("NotoSansCJKjp-Regular");
+    src:  local("SegoeUI-Italic"), local("SourceHanSans-Regular"), local("Noto-Sans-JP"), local("NotoSansJP-Regular"), local("NotoSansCJKjp-Regular");
 }
 @font-face {
     font-family: "Segoe UI";
     font-weight: 600;
     font-style: italic;
-    src:  local("SegoeUI-SemiboldItalic"), local("SourceHanSans-SemiBold"), local("NotoSansJP-SemiBold"), local("NotoSansCJKjp-SemiBold");
+    src:  local("SegoeUI-SemiboldItalic"), local("SourceHanSans-SemiBold"), local("Noto-Sans-JP"), local("NotoSansJP-SemiBold"), local("NotoSansCJKjp-SemiBold");
 }
 @font-face {
     font-family: "Segoe UI";
     font-weight: 700;
     font-style: italic;
-    src:  local("SegoeUI-BoldItalic"), local("SourceHanSans-Bold"), local("NotoSansJP-Bold"), local("NotoSansCJKjp-Bold");;
+    src:  local("SegoeUI-BoldItalic"), local("SourceHanSans-Bold"), local("Noto-Sans-JP"), local("NotoSansJP-Bold"), local("NotoSansCJKjp-Bold");;
 }
 EOF
